3e89fefa2c4974c91a8ab1998cec11ba26cc10d5
namith <namithjain171@gmail.com>
2024-01-26 08:05:58 +0530

    added env file

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose")
 // mongodb url
-// mongodb+srv://admin:69oT1OQIs4rwFtBb@to-do-app-cluster.niwc4rd.mongodb.net/todos
 
 //.env
-mongoose.connect("mongodb+srv://admin:69oT1OQIs4rwFtBb@to-do-app-cluster.niwc4rd.mongodb.net/todos")
+mongoose.connect(process.env.DB_HOST)
 
 const todoSchema = mongoose.Schema({
     title: String,
